66e33c031a269f435c373fca5ef3cd0878361286
bikeboardsurf1 <bikeboardsurf@protonmail.com>
2023-08-19 17:47:39 +0100

    Added trivy install

--- container-introduction/setup.sh
+++ container-introduction/setup.sh
@@ -22,3 +22,9 @@ server {
     }
 }
 EOL
+
+sudo apt-get install -y wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get -y update
+sudo apt-get install -y trivy
