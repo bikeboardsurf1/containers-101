bf30e08d515379ce041de82c7ef5edbfb2680048
bikeboardsurf1 <bikeboardsurf@protonmail.com>
2023-08-20 19:04:38 +0100

    Removed the nginx default config now using an index.html

--- container-introduction/setup.sh
+++ container-introduction/setup.sh
@@ -8,17 +8,26 @@ CMD ["/usr/sbin/nginx", "-g", "daemon off;"]
 EOL
 
 
-cat > /root/default <<EOL
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    
-    root /usr/share/nginx/html;
-    index index.html index.htm;
-
-    server_name _;
-    location / {
-        try_files $uri $uri/ =404;
-    }
+cat > /root/index.html <<EOL
+<!DOCTYPE html>
+<html>
+<head>
+<title>Containers 101</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+p.ex1 {
+  border: 1px solid green; 
+  padding: 20px;
+  background-color:MediumSeaGreen;
+  color: white;
+  font-size:250%;
 }
+
+</style>
+</head>
+<body>
+<br/><br/><br/><br/>
+<p class="ex1">C<i class="fa fa-circle-o" style="font-size:30px;color:red"></i>ntainers 1<i class="fa fa-circle-o" style="font-size:40px;color:red"></i>1</p>
+</body>
+</html>
 EOL
