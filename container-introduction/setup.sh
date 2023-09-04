7ab51f73fca4ad95a4f6151baf537fe30248b9c5
bikeboardsurf1 <bikeboardsurf@protonmail.com>
2023-09-04 20:48:12 +0100

    Fixed the nginx config

--- container-introduction/setup.sh
+++ container-introduction/setup.sh
@@ -61,15 +61,54 @@ CMD ["/usr/sbin/nginx", "-g", "daemon off;"]
 EOL
 
 cat > /root/containers101/step5/nginx.conf <<EOL
-server {
-    listen 8091 default_server;
-    listen [::]:8091 default_server; 
-    root /var/www/html;  
-    index index.html; 
-    server_name _;  
-    location / {
-       try_files $uri $uri/ =404;
-     }
+#user  nobody;
+worker_processes  1;
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+    #                  '$status $body_bytes_sent "$http_referer" '
+    #                  '"$http_user_agent" "$http_x_forwarded_for"';
+
+    #access_log  logs/access.log  main;
+
+    sendfile        on;
+
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    server {
+        listen       8085;
+        server_name  localhost;
+
+        #charset koi8-r;
+
+        #access_log  logs/host.access.log  main;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+
+        #error_page  404              /404.html;
+
+        # redirect server error pages to the static page /50x.html
+        #
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+
+    }
+
 }
 EOL
 
